968e57a3b1d433c8af019c0069095f05dd02389c
Tristan James McKinney <natsirtguy@gmail.com>
2018-11-13 13:38:57 -0800

    Write doc for the read_visa function.

--- src/h1b_counting.py
+++ src/h1b_counting.py
@@ -1,3 +1,18 @@
 '''Analyze H1B visa application information.'''
 
 from collections import Counter
+
+
+def read_visas(visas, fields):
+    '''Process a file object with visa application information.
+
+    Args:
+      visas: file_object, first line has fields of visa information and
+        remaining lines contain instances of applications.
+      fields: tuple with strings of fields to be processed.
+
+    Returns:
+      top10s: tuple containing strings with information on the top 10
+        most common occurences of fields in the visa file.
+    '''
+    return
