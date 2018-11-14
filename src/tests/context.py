ebaa18738f318bc696faab8113a6f23900d89f61
Tristan James McKinney <natsirtguy@gmail.com>
2018-11-13 19:01:59 -0800

    Move pytest tests into src.

--- src/tests/context.py
+++ src/tests/context.py
@@ -2,6 +2,6 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath(
-    os.path.join(os.path.dirname(__file__), '../../..')))
+    os.path.join(os.path.dirname(__file__), '../..')))
 
 import src.h1b_counting as h1b_counting
