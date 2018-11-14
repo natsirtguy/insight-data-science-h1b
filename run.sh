714d4e5b2603fc9112a8f5ada5599fba96c75920
Tristan James McKinney <natsirtguy@gmail.com>
2018-11-13 16:58:19 -0800

    Give more information in run.sh.

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 #
-# Execute the python script to generate the occupation and state files.
+# Execute the python script to investigate the top 10 most common
+# occupations and states for certified H1B visa applications.
+#
+# To perform the analysis on a new dataset, place it in the ./input/
+# directory and name the file h1b_input.csv.
 
 python3 ./src/h1b_script.py ./input/h1b_input.csv ./output/top_10_occupations.txt ./output/top_10_states.txt
