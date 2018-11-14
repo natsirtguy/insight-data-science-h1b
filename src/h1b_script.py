6cf135ea899236f30f7e914df39fc9876110e3b6
Tristan James McKinney <natsirtguy@gmail.com>
2018-11-13 18:00:33 -0800

    Deal with case where worksite_state has a different name.

--- src/h1b_script.py
+++ src/h1b_script.py
@@ -1,7 +1,7 @@
 '''Script to analyze H1B visa application information.'''
 import sys
 from getopt import getopt
-from h1b_counting import read_visas
+from h1b_counting import read_visas, MissingFieldError
 
 
 if __name__ == "__main__":
@@ -19,11 +19,21 @@ else:
           'output_file1 output_file2.')
     exit()
 
-with open(input_file, 'r') as visas:
-    top_10s = read_visas(visas,
-                         {'SOC_NAME': 'OCCUPATIONS',
-                          'WORKSITE_STATE': 'STATES'})
-    with open(output_file1, 'w') as occupation_file:
-        occupation_file.write(top_10s['OCCUPATIONS'])
-    with open(output_file2, 'w') as state_file:
-        state_file.write(top_10s['STATES'])
+try:
+    with open(input_file, 'r') as visas:
+        top_10s = read_visas(visas,
+                             {'SOC_NAME': 'OCCUPATIONS',
+                              'WORKSITE_STATE': 'STATES'})
+        with open(output_file1, 'w') as occupation_file:
+            occupation_file.write(top_10s['OCCUPATIONS'])
+        with open(output_file2, 'w') as state_file:
+            state_file.write(top_10s['STATES'])
+except MissingFieldError:
+    with open(input_file, 'r') as visas:
+        top_10s = read_visas(visas,
+                             {'SOC_NAME': 'OCCUPATIONS',
+                              'WORKLOC1_STATE': 'STATES'})
+        with open(output_file1, 'w') as occupation_file:
+            occupation_file.write(top_10s['OCCUPATIONS'])
+        with open(output_file2, 'w') as state_file:
+            state_file.write(top_10s['STATES'])
