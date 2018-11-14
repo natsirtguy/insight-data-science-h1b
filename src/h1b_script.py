f307ededb85bcd0fe96b91acd65af34273802967
Tristan James McKinney <natsirtguy@gmail.com>
2018-11-13 16:41:21 -0800

    Check for correct number of input, output files.

--- src/h1b_script.py
+++ src/h1b_script.py
@@ -8,9 +8,10 @@ if __name__ == "__main__":
     opts, input_output_files = getopt(sys.argv[1:], '')
     try:
         input_file = input_output_files[0]
-        output_files = input_output_files[1:]
+        output_file1 = input_output_files[1]
+        output_file2 = input_output_files[2]
     except IndexError:
-        print('Plese specify input and output files.')
+        print('Plese specify one input and two output files.')
         exit()
 else:
     print('This program is meant to be run as a script. '
@@ -22,7 +23,7 @@ with open(input_file, 'r') as visas:
     top_10s = read_visas(visas,
                          {'SOC_NAME': 'OCCUPATIONS',
                           'WORKSITE_STATE': 'STATES'})
-    with open(output_files[0], 'w') as occupation_file:
+    with open(output_file1, 'w') as occupation_file:
         occupation_file.write(top_10s['OCCUPATIONS'])
-    with open(output_files[1], 'w') as state_file:
+    with open(output_file2, 'w') as state_file:
         state_file.write(top_10s['STATES'])
